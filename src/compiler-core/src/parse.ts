de0817dd42b5a4e5c72cc6770b599335715f2de0
caijianhaoa <1784581164@qq.com>
2022-05-01 21:29:38 +0800

    实现解析插值语法,element标签,text功能

---
+++ src/compiler-core/src/parse.ts
@@ -0,0 +1,145 @@
+import { NodeTypes } from './ast'
+
+const enum TagType {
+  Start,
+  End,
+}
+
+export function baseParse(content: string) {
+  const context = createParseContext(content)
+  return createRoot(parseChildren(context, []))
+}
+
+function parseChildren(context, ancestors) {
+  const nodes: any[] = []
+  while (!isEnd(context, ancestors)) {
+    let node
+    const s = context.source
+    if (s.startsWith('{{')) {
+      node = parseInterpolation(context)
+    } else if (s[0] === '<') {
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors)
+      }
+    }
+    if (!node) {
+      node = parseText(context)
+    }
+    nodes.push(node)
+  }
+  return nodes
+}
+
+function isEnd(context, ancestors) {
+  // 1.当遇到结束标签的时候
+  const s = context.source
+  if (s.startsWith('</')) {
+    for (let i = ancestors.length - 1; i >= 0; i--) {
+      const tag = ancestors[i].tag
+      if (startsWithEndTagOpen(s, tag)) {
+        return true
+      }
+    }
+  }
+  // 2.source 没有值的时候
+  return !context.source
+}
+
+function parseText(context) {
+  let endIndex = context.source.length
+  let endToken = ['<', '{{']
+  for (let i = 0; i < endToken.length; i++) {
+    const index = context.source.indexOf(endToken[i])
+    if (index !== -1 && endIndex > index) {
+      endIndex = index
+    }
+  }
+
+  // 1.获取content
+  const content = parseTextData(context, endIndex)
+  console.log('--------', content)
+  return {
+    type: NodeTypes.TEXT,
+    content,
+  }
+}
+
+function parseTextData(context, length) {
+  const content = context.source.slice(0, length)
+  // 推进
+  advanceBy(context, length)
+  return content
+}
+
+function parseElement(context: any, ancestors) {
+  // 解析tag
+  const element: any = parseTag(context, TagType.Start)
+  ancestors.push(element)
+  element.children = parseChildren(context, ancestors)
+  ancestors.pop()
+  if (startsWithEndTagOpen(context.source, element.tag)) {
+    parseTag(context, TagType.End)
+  } else {
+    throw new Error(`缺少结束标签:${element.tag}`)
+  }
+  console.log('--------', context.source)
+  return element
+}
+
+function startsWithEndTagOpen(source, tag) {
+  return (
+    source.startsWith('</') &&
+    source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
+  )
+}
+
+function parseTag(context, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+  // 删除处理完成的代码
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+  if (type === TagType.End) return
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  }
+}
+
+function parseInterpolation(context) {
+  // {{message}}
+  const openDelimiter = '{{'
+  const closeDelimiter = '}}'
+  const closeIndex = context.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  )
+  advanceBy(context, openDelimiter.length)
+  const rawContentLength = closeIndex - openDelimiter.length
+  const rawContent = parseTextData(context, rawContentLength)
+  const content = rawContent.trim()
+  advanceBy(context, closeDelimiter.length)
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content,
+    },
+  }
+}
+
+function advanceBy(context: any, length: number) {
+  context.source = context.source.slice(length)
+}
+
+function createRoot(children) {
+  return {
+    children,
+  }
+}
+
+function createParseContext(content: string) {
+  return {
+    source: content,
+  }
+}
