735b89116c24a579668732012f9722dc4206f005
caijianhaoa <1784581164@qq.com>
2022-09-30 14:45:41 +0800

    添加tsup打包并修改文档

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  // 后续会增加 entry
+  entry: {
+    index: 'packages/vue/src/index.ts',
+  },
+  outDir: './packages/vue/build',
+  // 产物格式，包含 esm 和 cjs 格式
+  format: ['esm', 'cjs'],
+  // 目标语法
+  target: 'es2020',
+  // 生成 sourcemap
+  sourcemap: true,
+  // 没有拆包的需求，关闭拆包能力
+  splitting: false,
+})
