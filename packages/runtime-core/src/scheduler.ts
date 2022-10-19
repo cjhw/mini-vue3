89aef779277a129947a2b9c2e87ff2c6834e7a6b
caijianhaoa <1784581164@qq.com>
2022-10-19 23:30:27 +0800

    实现watchEffect

--- packages/runtime-core/src/scheduler.ts
+++ packages/runtime-core/src/scheduler.ts
@@ -1,9 +1,10 @@
 const queue: any[] = []
+const activePreFlushCbs: any[] = []
 
 const p = Promise.resolve()
 let isFlushPending = false
 
-export function nextTick(fn) {
+export function nextTick(fn?) {
   return fn ? p.then(fn) : p
 }
 
@@ -11,6 +12,12 @@ export function queueJobs(job) {
   if (!queue.includes(job)) {
     queue.push(job)
   }
+  queueFlush()
+}
+
+export function queuePreFlushCb(job) {
+  activePreFlushCbs.push(job)
+
   queueFlush()
 }
 
@@ -22,8 +29,17 @@ function queueFlush() {
 
 function flushJobs() {
   isFlushPending = false
+
+  flushPreFlushCbs()
+
   let job
   while ((job = queue.shift())) {
     job && job()
+  }
+}
+
+function flushPreFlushCbs() {
+  for (let i = 0; i < activePreFlushCbs.length; i++) {
+    activePreFlushCbs[i]()
   }
 }
