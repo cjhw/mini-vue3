a7b40a82f9fd778158dce1e59222774358219e67
caijianhao <caijianhao@yy.com>
2023-01-09 11:33:04 +0800

    docs: update README.md

--- filetoc.config.js
+++ filetoc.config.js
@@ -1,8 +1,8 @@
 // filetoc.config.js
 module.exports = {
-  remoteUrl: 'https://github.com/caijianhaoa/mini-vue3', // your repo address
-  mainBranch: 'main', // your default branch. default 'main'
-  dirPath: '.', //  the dir where you want to gengerate the toc. default '.'
-  mdPath: ['./README.md', './README.zh.md'], // the markdown files path, when there is only one path, it also can be a string.  default ['README.md']
-  excludes: ['.git', 'node_modules'], // the excludes file name or dir name
-}
+  remoteUrl: "https://github.com/cjhw/mini-vue3", // your repo address
+  mainBranch: "main", // your default branch. default 'main'
+  dirPath: ".", //  the dir where you want to gengerate the toc. default '.'
+  mdPath: ["./README.md", "./README.zh.md"], // the markdown files path, when there is only one path, it also can be a string.  default ['README.md']
+  excludes: [".git", "node_modules"], // the excludes file name or dir name
+};
